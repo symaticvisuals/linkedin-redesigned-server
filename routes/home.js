e272e9c757efbe9a932c5fc3cf7ea76f606cc3d2
unknown <samdragneal@gmail.com>
2021-12-15 11:32:14 +0530

    lol

--- routes/home.js
+++ routes/home.js
@@ -4,5 +4,8 @@ const router = express.Router();
 router.get("/", (req, res) => {
 	res.send("Home Page");
 });
+router.post("/", (req, res) => {
+	res.send("Home Page");
+});
 
 module.exports = router;
