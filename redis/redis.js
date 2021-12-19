a800c34e8bb110a9568fb85b63498f3d898bc167
unknown <samdragneal@gmail.com>
2021-12-19 09:41:28 +0530

    fix: redis timer fixed, user get by id fixed

--- redis/redis.js
+++ redis/redis.js
@@ -1,17 +1,22 @@
 //io redis is a redis client for node
 const Redis = require('ioredis');
 require('dotenv').config();
-const url = require('url')
 
 const REDIS_URL = process.env.NOMI_REDIS_ENDPOINT;
 
+// uses heroku redis endpoint
+//  >>>for testing
 
-const client = new Redis(process.env.NOMI_REDIS_ENDPOINT,{
+const client = new Redis(REDIS_URL, {
     tls: {
         rejectUnauthorized: false,
         enableTLSForSentinelMode: true
-     },
-}); //uses default configuration i.e localhost and port 6379
+    },
+});
+
+//uses default configuration i.e localhost and port 6379
+// >>>for development
+// const client = new Redis();
 
 module.exports = {
     client
