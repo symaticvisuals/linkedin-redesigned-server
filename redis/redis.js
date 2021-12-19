87c0ae2165a51158204f6d1c0655d7336bd08c43
Deepanshu Goel <59753906+symaticvisuals@users.noreply.github.com>
2021-12-19 11:49:33 +0530

    fix: redis

--- redis/redis.js
+++ redis/redis.js
@@ -1,23 +1,23 @@
 //io redis is a redis client for node
-const Redis = require('ioredis');
-require('dotenv').config();
+const Redis = require("ioredis");
+require("dotenv").config();
 
 const REDIS_URL = process.env.NOMI_REDIS_ENDPOINT;
 
 // uses heroku redis endpoint
 //  >>>for testing
 
-// const client = new Redis(REDIS_URL,{
-//     tls: {
-//         rejectUnauthorized: false,
-//         enableTLSForSentinelMode: true
-//      },
-// });
+const client = new Redis(REDIS_URL, {
+	tls: {
+		rejectUnauthorized: false,
+		enableTLSForSentinelMode: true,
+	},
+});
 
 //uses default configuration i.e localhost and port 6379
 // >>>for development
-const client = new Redis();
+// const client = new Redis();
 
 module.exports = {
-    client
-}
+	client,
+};
