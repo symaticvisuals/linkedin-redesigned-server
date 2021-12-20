1e83bd997140565f2bcb0d4ff1b53673a5e8d2c1
Deepanshu Goel <59753906+symaticvisuals@users.noreply.github.com>
2021-12-20 15:34:20 +0530

    fix corsv2

--- controller/auth.js
+++ controller/auth.js
@@ -1,30 +1,51 @@
-const config = require('../utils/config');
-const userJwt = require('../classes/user/functions');
-const adminJwt = require('../classes/admin/functions');
-const utils = require('../utils/utils');
-const messageBundle = require('../locales/en');
+const config = require("../utils/config");
+const userJwt = require("../classes/user/functions");
+const adminJwt = require("../classes/admin/functions");
+const utils = require("../utils/utils");
+const messageBundle = require("../locales/en");
 
 exports.isUserJwt = async (req, res, next) => {
-    try {
-        let jwtToken = req.body.jwtToken || req.cookies.access_token || req.headers["access_token"];
-        // console.log(jwtToken.data);
-        let payload = await userJwt.decodeJwt(jwtToken);
-        payload.data.role = config.ACCESS.USER;
+	try {
+		let jwtToken =
+			req.body.jwtToken ||
+			req.cookies.access_token ||
+			req.headers["access_token"];
+		console.log(jwtToken);
+		console.log(req.headers);
+		let payload = await userJwt.decodeJwt(jwtToken);
+		payload.data.role = config.ACCESS.USER;
 
-        req.user = payload.data;
-        next();
-    } catch (err) {
-        return utils.sendResponse(req, res, false, messageBundle['jwtDecode.fail'], {}, err);
-    }
-}
+		req.user = payload.data;
+		next();
+	} catch (err) {
+		return utils.sendResponse(
+			req,
+			res,
+			false,
+			messageBundle["jwtDecode.fail"],
+			{},
+			err
+		);
+	}
+};
 exports.isAdminJwt = async (req, res, next) => {
-    try {
-        let jwtToken = req.body.jwtToken || req.cookies.access_token || req.headers["access_token"];
-        let payload = await adminJwt.decodeToken(jwtToken);
-        payload.data.role = config.ACCESS.ADMIN;
-        req.user = payload.data;
-        next();
-    } catch (err) {
-        return utils.sendResponse(req, res, false, messageBundle['jwtDecode.fail'], {}, err);
-    }
-}
+	try {
+		let jwtToken =
+			req.body.jwtToken ||
+			req.cookies.access_token ||
+			req.headers["access_token"];
+		let payload = await adminJwt.decodeToken(jwtToken);
+		payload.data.role = config.ACCESS.ADMIN;
+		req.user = payload.data;
+		next();
+	} catch (err) {
+		return utils.sendResponse(
+			req,
+			res,
+			false,
+			messageBundle["jwtDecode.fail"],
+			{},
+			err
+		);
+	}
+};
